0d9031d9e377c44dfd9460a1c4159eec46b2c00e
huseynabishov <92877169+huseynabishov@users.noreply.github.com>
2022-04-18 23:53:47 +0300

    Initial Commit

---
+++ SightseeingGuide/SightseeingGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SightseeingGuideApp.swift
+//  SightseeingGuide
+//
+//  Created by Huseyn Abishov on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SightseeingGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
