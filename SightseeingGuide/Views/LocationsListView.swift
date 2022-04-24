b3265bc836c754a0147ee5fbc227e61c887cfc8b
huseynabishov <92877169+huseynabishov@users.noreply.github.com>
2022-04-24 23:32:51 +0300

    Locations List

---
+++ SightseeingGuide/Views/LocationsListView.swift
@@ -0,0 +1,54 @@
+//
+//  LocationsListView.swift
+//  SightseeingGuide
+//
+//  Created by Huseyn Abishov on 24.04.2022.
+//
+
+import SwiftUI
+
+struct LocationsListView: View {
+    
+    @EnvironmentObject private var vm: LocationsViewModel
+    
+    var body: some View {
+        List {
+            ForEach(vm.locations) { location in
+                listRowView(location: location)
+                    .padding(.vertical, 4)
+                    .listRowBackground(Color.clear)
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+}
+
+struct LocationsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsListView()
+    }
+}
+
+extension LocationsListView {
+    
+    private func listRowView(location: Location) -> some View{
+        HStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 45, height: 45)
+                    .cornerRadius(10)
+            }
+            
+            VStack(alignment: .leading) {
+                Text(location.name)
+                    .font(.headline)
+                Text(location.cityName)
+                    .font(.subheadline)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+    
+}
--- SightseeingGuide/Views/LocationsView.swift
+++ SightseeingGuide/Views/LocationsView.swift
@@ -16,6 +16,13 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                
+                Spacer()
+            }
         }
     }
 }
@@ -24,5 +31,33 @@ struct LocationsView_Previews: PreviewProvider {
     static var previews: some View {
         LocationsView()
             .environmentObject(LocationsViewModel())
+    }
+}
+
+extension LocationsView {
+    
+    private var header: some View {
+    VStack {
+        Button(action: vm.toggleLocationsList) {
+          Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+            .font(.title2)
+            .fontWeight(.black)
+            .foregroundColor(.primary)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .overlay(alignment: .leading) {
+                Image(systemName: "arrow.down")
+                    .font(.headline)
+                    .foregroundColor(.primary)
+                    .padding()
+             }
+        }
+        if vm.showLocationsList {
+            LocationsListView()
+        }
+    }
+    .background(.thickMaterial)
+    .cornerRadius(10)
+    .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
     }
 }
